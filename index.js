2c6d94b76579ccf5e40468c259493412eac121a6
Restyled.io <commits@restyled.io>
2020-10-04 16:33:00 +0000

    Restyled by clang-format

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 $(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
+  $('a.page-scroll').bind('click', function(event) {
+    var $anchor = $(this);
+    $('html, body')
+        .stop()
+        .animate({scrollTop : $($anchor.attr('href')).offset().top}, 1500,
+                 'easeInOutExpo');
+    event.preventDefault();
+  });
 });
-
 
 function myFunction() {
   var x = document.getElementById("myTopnav");
