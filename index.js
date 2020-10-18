e8ab61bd3d90659c959e8d63353af7207103c78c
chamindujs <chamindujs@gmail.com>
2020-10-18 22:02:37 +0530

    Re designed the navbar to be Bootstrap free

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ $(function() {
 });
 
 
-// Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
-});
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
